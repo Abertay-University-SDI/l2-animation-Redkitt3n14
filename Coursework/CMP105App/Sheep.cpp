1afdf9ea51d566926d95fb9521425371f0d71837
Redkitt3n14 <69162335+Redkitt3n14@users.noreply.github.com>
2026-02-02 20:33:31 +0000

    The sheep moves now

--- Coursework/CMP105App/Sheep.cpp
+++ Coursework/CMP105App/Sheep.cpp
@@ -1,14 +1,92 @@
 #include "Sheep.h"
+#include "Framework/animation.h"
 
 Sheep::Sheep()
 {
-	//animation
+	//initialise animations
+	//*Animation m_currentAnimation;
+	Animation m_walkDown;
 }
 
 void Sheep::update(float dt)
 {
+	// set sheep direction
+	// 
+		// for diagonal movement
+	float diagonal_speed = m_speed * APPROX_ONE_OVER_ROOT_TWO * dt;
+	float orthog_speed = m_speed * dt;	// orthogonal movement
+
+	switch (m_direction)
+	{
+	case Direction::UP:
+		move({ 0, -orthog_speed });
+		break;
+	case Direction::UP_RIGHT:
+		move({ diagonal_speed, -diagonal_speed });
+		break;
+	case Direction::RIGHT:
+		move({ orthog_speed,0 });
+		break;
+	case Direction::DOWN_RIGHT:
+		move({ diagonal_speed, diagonal_speed });
+		break;
+	case Direction::DOWN:
+		move({ 0, orthog_speed });
+		break;
+	case Direction::DOWN_LEFT:
+		move({ -diagonal_speed, diagonal_speed });
+		break;
+	case Direction::LEFT:
+		move({ -orthog_speed,0 });
+		break;
+	case Direction::UP_LEFT:
+		move({ -diagonal_speed, -diagonal_speed });
+		break;
+	}
 }
 
 void Sheep::handleInput(float dt)
 {
+	// move the sheep
+	// 
+	// decrement and check the input buffer.
+	m_inputBuffer -= dt;
+	if (m_inputBuffer > 0)
+	{
+		// not long enough has passed since the last input change, so don't handle input
+		return;
+	}
+	// grab this to detect changes per frame for later
+	Direction last_dir = m_direction;
+
+	// Set 8-directional movement based on WASD
+	if (m_input->isKeyDown(sf::Keyboard::Scancode::A))
+	{
+		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP_LEFT;
+		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN_LEFT;
+		else
+			m_direction = Direction::LEFT;
+	}
+	else if (m_input->isKeyDown(sf::Keyboard::Scancode::D))
+	{
+		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP_RIGHT;
+		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN_RIGHT;
+		else
+			m_direction = Direction::RIGHT;
+	}
+	else
+	{
+		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP;
+		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN;
+	}
+
+	// set input buffer if needed, this makes diagonal movement easier
+	if (m_direction != last_dir)
+		m_inputBuffer = INPUT_BUFFER_LENGTH;
 }
--- Coursework/CMP105App/Sheep.h
+++ Coursework/CMP105App/Sheep.h
@@ -1,14 +1,25 @@
 #pragma once
 #include "Framework/GameObject.h"
 #include "Framework/Animation.h"
+
 class Sheep : public GameObject
 {
+
 public:
 	Sheep();
-	virtual ~Sheep() = default;
+	~Sheep() = default;
 
 	void update(float dt) override;
 	void handleInput(float dt) override;
+
+
 private:
 	// Sheep member variables here
+	enum class Direction { UP, DOWN, LEFT, RIGHT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT, NONE };
+	Direction m_direction = Direction::NONE;
+	float m_speed = 300.0f;
+	float m_inputBuffer = 0.f;
+
+	const float INPUT_BUFFER_LENGTH = 0.1f;
+	const float APPROX_ONE_OVER_ROOT_TWO = 0.70710678f;	// 1 / sqrt(2)
 };
